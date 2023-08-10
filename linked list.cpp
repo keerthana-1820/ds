1ed4658585540c7559955147d1b932bbb69c31a1
keerthana-1820 <123346947+keerthana-1820@users.noreply.github.com>
2023-08-10 09:08:40 +0530

    Add files via upload

---
+++ linked list.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to insert a new node at the beginning of the linked list
+void insertAtBeginning(struct Node** head, int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Function to insert a new node at the end of the linked list
+void insertAtEnd(struct Node** head, int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = newNode;
+}
+
+// Function to delete a node with a given value from the linked list
+void deleteNode(struct Node** head, int key) {
+    struct Node* temp = *head;
+    struct Node* prev = NULL;
+
+    if (temp != NULL && temp->data == key) {
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Element not found in the linked list.\n");
+        return;
+    }
+
+    prev->next = temp->next;
+    free(temp);
+}
+
+// Function to display the linked list
+void display(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insertAtEnd(&head, 10);
+    insertAtBeginning(&head, 5);
+    insertAtEnd(&head, 20);
+    insertAtBeginning(&head, 2);
+
+    printf("Linked list: ");
+    display(head);
+
+    deleteNode(&head, 5);
+    deleteNode(&head, 15);
+
+    printf("Linked list after deletion: ");
+    display(head);
+
+    return 0;
+}
+
