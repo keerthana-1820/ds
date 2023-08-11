8a898ae4e3fa49861ec352f9f7756fefaf01b0f5
keerthana-1820 <123346947+keerthana-1820@users.noreply.github.com>
2023-08-11 08:01:52 +0530

    Add files via upload

---
+++ quick sort.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+// Function to partition the array into two subarrays
+// such that elements smaller than pivot are on the left
+// and elements greater than pivot are on the right
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+// Function to perform quick sort
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+// Function to display an array
+void displayArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    displayArray(arr, size);
+
+    quickSort(arr, 0, size - 1);
+
+    printf("Array after quick sort: ");
+    displayArray(arr, size);
+
+    return 0;
+}
+
